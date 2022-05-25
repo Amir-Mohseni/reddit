a00970d97b5f748989ca89ecb2626ee953d191be
amirsepehr1381 <abediniamirsepehr@gmail.com>
2022-05-25 11:50:05 +0430

    card added to loginpage

--- lib/Login/LoginPage.dart
+++ lib/Login/LoginPage.dart
@@ -31,70 +31,86 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.fromLTRB(0, 50, 0, 10),
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 5.0),
-              margin: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 8.0),
-              child: TextField(
-                controller: namec,
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                    errorText: userError,
-                    hintText: "Username",
-                    contentPadding:
-                    const EdgeInsets.fromLTRB(16.0, 14.0, 16.0, 14.0),
-                    border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 5.0),
-              margin: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 8.0),
-              child: TextField(
-                controller: pasc,
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                    hintText: "Password",
-                    errorText: desError,
-                    contentPadding:
-                    const EdgeInsets.fromLTRB(16.0, 14.0, 16.0, 14.0),
-                    border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 8.0),
-              child: MaterialButton(
-                padding: const EdgeInsets.fromLTRB(16.0, 14.0, 16.0, 14.0),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                color: Colors.blueAccent,
-                onPressed: () {
-                  FocusScope.of(context).requestFocus(FocusNode());
-                  setState(() {
-                    if (namec.text.length < 3) {
-                      userError = "username must be > 4";
-                    } else if (pasc.text.length < 4) {
-                      userError = "";
-                      desError = "password must be > 4";
-                    } else {
-                      userError = "";
-                      desError = "";
-                      Future.delayed(Duration(milliseconds: 500), () {
-                        Navigator.pop(context);
-                      });
-                    }
-                  });
-                },
-                child: const Text(
-                  "ADD",
-                  style: TextStyle(color: Colors.white),
+        padding: EdgeInsets.fromLTRB(50, 50, 50, 400),
+        child: SafeArea(
+          child: Card(
+            key: Key('loginCard'),
+            elevation: 10,
+            borderOnForeground: true,
+            color: Colors.blue,
+            margin: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Expanded(
+                  key: Key('username'),
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 5.0),
+                    child: TextField(
+                      controller: namec,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                          errorText: userError,
+                          hintText: "Username",
+                          contentPadding:
+                          const EdgeInsets.fromLTRB(16.0, 14.0, 16.0, 14.0),
+                          border: const OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+                    ),
+                  ),
+                ),
+              Expanded(
+                key: Key('password'),
+                child:
+              Container(
+                padding: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 5.0),
+              //    margin: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 8.0),
+                  child: TextField(
+                    controller: pasc,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                        hintText: "Password",
+                        errorText: desError,
+                        contentPadding:
+                        const EdgeInsets.fromLTRB(16.0, 14.0, 16.0, 14.0),
+                        border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+                  ),
                 ),
               ),
-            )
-          ],
+                Container(
+                    key: Key('loginButton'),
+                   // margin: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 8.0),
+                    child: MaterialButton(
+                     // padding: const EdgeInsets.fromLTRB(16.0, 14.0, 16.0, 14.0),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                      color: Colors.blueAccent,
+                      onPressed: () {
+                        FocusScope.of(context).requestFocus(FocusNode());
+                        setState(() {
+                          if (namec.text.length < 3) {
+                            userError = "username must be > 4";
+                          } else if (pasc.text.length < 4) {
+                            userError = "";
+                            desError = "password must be > 4";
+                          } else {
+                            userError = "";
+                            desError = "";
+                            Future.delayed(Duration(milliseconds: 500), () {
+                              Navigator.pop(context);
+                            });
+                          }
+                        });
+                      },
+                      child: const Text(
+                        "ADD",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
         ),
       ),
     );
